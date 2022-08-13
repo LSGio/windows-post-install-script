16635e1a6e9addfee35c48316923f535f80ca991
LSGio <shvartzlior@gmail.com>
2022-08-13 19:33:16 +0300

    added missing documentation

--- WindowsUtils.py
+++ WindowsUtils.py
@@ -1,4 +1,3 @@
-
 import ctypes
 from os.path import join
 import RegUtils
@@ -8,8 +7,8 @@ import sys
 def request_elevated_permissions() -> None:
     """
     Prompt the user to re-run the program as admin.
-
     """
+
     if not is_running_as_admin():
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
 
@@ -21,11 +20,19 @@ def is_running_as_admin() -> bool:
     :rtype: bool
     :return: True if current process is running as admin, and False otherwise
     """
+
     return ctypes.windll.shell32.IsUserAnAdmin() == 1
 
 
-def get_windows_build_number():
+def get_windows_build_number() -> str:
+    """
+    Get the current windows build version.
+
+    :rtype: str
+    :return: The current build version
+    """
+
     root_path = RegUtils.HKLM
     key_path = join('SOFTWARE', 'Microsoft', 'Windows NT', 'CurrentVersion')
-    value_name = 'CurrentBuildNumber'
-    return int(RegUtils.get_value_of_reg_key(root_path, key_path, value_name))
+    value_name = 'DisplayVersion'
+    return str(RegUtils.get_value_of_reg_key(root_path, key_path, value_name))
